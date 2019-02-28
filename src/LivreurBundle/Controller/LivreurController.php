63cf2588152cae352073bc3410aba87d544cb4f2
medraies97 <48074657+medraies97@users.noreply.github.com>
2019-02-28 03:42:53 +0100

    livreur raies

--- src/LivreurBundle/Controller/LivreurController.php
+++ src/LivreurBundle/Controller/LivreurController.php
@@ -13,10 +13,5 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class LivreurController extends Controller
 {
-    public function LivreurListAction()
-    {
-        $em=$this->getDoctrine()->getManager();
-        $livreur=$em->getRepository("LivreurBundle:Livreur")->findAll();
-        return $this->render("@Livreur\Livreur\listLivreur.html.twig", array('livreur'=>$livreur));
-    }
+
 }
